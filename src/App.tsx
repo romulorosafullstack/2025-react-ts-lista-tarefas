2c1e6fd7f56a5337e0bdb103a478303153067592
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-24 14:29:12 -0300

    tasks hook useState e map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
 export default function App() {
+  const [tasks, setTasks] = useState([
+    'Estudar JavaScript',
+    'Estudar TypeScript',
+    'Comprar React + TypeScript',
+  ])
+
   return (
     <>
-      <h1>Lista de Tarefas</h1>
+      <h1>Lista de Nivelamento Dev | Smartts Utilities</h1>
+
+      <ul>
+        {/* Percorro o array */}
+        {tasks.map((task, index) => (
+          // key é um identificador único para cada item da lista
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </>
   )
 }
