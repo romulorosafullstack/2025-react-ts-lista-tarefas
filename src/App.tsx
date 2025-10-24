3692a18181a0c5bfbde283a7502670e3c6b94ff7
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-24 14:58:55 -0300

    handleRegister + botao + excluir item

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import { useState } from "react"
 
 export default function App() {
+  const [input, setInput] = useState("")
   const [tasks, setTasks] = useState([
     'Estudar JavaScript',
     'Estudar TypeScript',
     'Comprar React + TypeScript',
   ])
 
+  function handleRegister() {
+    if (!input) {
+      alert("O nome da tarefa não pode estar vazio!")
+      return
+    } else {
+      // Adiciono a nova tarefa ao array
+      setTasks([...tasks, input])
+      // Limpo o input
+      setInput("")
+    }
+  }
+
   return (
     <>
       <h1>Lista de Nivelamento Dev | Smartts Utilities</h1>
 
-      <ul>
-        {/* Percorro o array */}
-        {tasks.map((task, index) => (
-          // key é um identificador único para cada item da lista
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      <input
+        placeholder="Digite o nome da tarefa"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleRegister}>
+        Adicionar Task
+      </button>
+
+      <p>Nome da Tarefa: {input}</p>
+
+      {tasks.map((task, index) => (
+      <section>
+        {/* key é um identificador único para cada item da lista */}
+        <span key={index}>{task}</span>
+        <button>Excluir</button>
+      </section>
+      ))}
     </>
   )
 }
